90792a36054de76ca3b9ee2d02a609d2ce3c2f5e
Alperen <alperensafak@gmail.com>
2021-05-01 01:29:26 +0300

    added prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function HomePage() {
-    return <div className="text-blue-400">Welcome to Next.js!</div>
+  return <div className="text-blue-400">Welcome to Next.js!</div>
 }
 
 export default HomePage
